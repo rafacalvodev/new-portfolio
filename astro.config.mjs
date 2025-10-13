56c4b1b3a11eca5391ebdaf68b62ddcdb9dcb89b
Rafael Calvo <rafacalvodev@gmail.com>
2025-10-13 09:16:54 -0400

    Initial Commit
    First Commit with Navbar fully configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: true,
+  site: 'http://localhost:4321/',
+  integrations: [mdx(), icon()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => {},
+          },
+        },
+      },
+    },
+  }, 
+});
